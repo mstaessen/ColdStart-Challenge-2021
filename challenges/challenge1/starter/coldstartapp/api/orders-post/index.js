f12b970021657ea83fc511bdc9a21af0911ce53c
Michiel Staessen <staessenmichiel@gmail.com>
2021-02-27 17:15:58 +0100

    don't send back error details

--- challenges/challenge1/starter/coldstartapp/api/orders-post/index.js
+++ challenges/challenge1/starter/coldstartapp/api/orders-post/index.js
@@ -10,24 +10,16 @@ module.exports = async function (context, req) {
     return;
   }
 
-  try {
-    context.bindings.preOrder = {
-      Id: uuidv4(),
-      User: user.userDetails,
-      Date: Date.now,
-      IcecreamId: req.body.productId,
-      Status: 'New',
-      DriverId: null,
-      FullAddress: '1 Microsoft Way, Redmond, WA 98052, USA',
-      LastPosition: null
-    };
-  } catch (error) {
-    context.res = {
-      status: 500,
-      body: error
-    };
-    return;
-  }
+  context.bindings.preOrder = {
+    Id: uuidv4(),
+    User: user.userDetails,
+    Date: Date.now,
+    IcecreamId: req.body.productId,
+    Status: 'New',
+    DriverId: null,
+    FullAddress: '1 Microsoft Way, Redmond, WA 98052, USA',
+    LastPosition: null
+  };
 
   context.res.status(201);
 };
